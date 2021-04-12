26a34ab2a4eab7d482b7c7dc830ef76e4214e30a
Bittah Swami <41178536+6democratickim9@users.noreply.github.com>
2021-04-13 08:41:31 +0900

    Presentation Error

---
+++ kim/issue5.py
@@ -0,0 +1,36 @@
+def stars(p1, p2):
+
+    if p1<=0 or p1>100 or p2<1 or p2>3:
+        print("INPUT ERROR!")
+        n, m = input("").split(' ')
+
+        n = int(n)
+        m = int(m)
+
+        stars(n, m)
+            
+
+
+    else:
+        if p2 == 1:
+            for i in range(1,p1+1):
+                print("*"*i)
+            print()
+
+        elif p2 == 2:
+            for i in range(p1, 0, -1):
+                print("*"*i)
+            print()
+        
+
+        elif p2 == 3:
+            for i in range(1, p1+1):
+                print (" "*(p1-i),"*"*(2*i-1))
+            print()
+
+n, m = input("").split(' ')
+
+n = int(n)
+m = int(m)
+
+stars(n, m)
